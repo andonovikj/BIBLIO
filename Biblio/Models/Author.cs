7783d2b87dc825c36d6dd543e7f482d2232e5ac8
Mila <andonovikjmila@gmail.com>
2022-10-14 12:20:55 +0200

    added models

---
+++ Biblio/Models/Author.cs
@@ -0,0 +1,15 @@
+﻿namespace Biblio.Models
+{
+    public class Author
+    {
+        public int Id { get; set; }
+        public string Name { get; set; }
+        public string Bio { get; set; }
+        public string ProfilePictureURL { get; set; }
+
+        //enum
+        public Genre genre { get; set; }
+
+        public List<Book> books { get; set; }
+    }
+}
---
+++ Biblio/Models/Book.cs
@@ -0,0 +1,19 @@
+﻿namespace Biblio.Models
+{
+    public class Book
+    {
+        public int Id { get; set; }
+        public string Title { get; set; }
+        
+        public string Description { get; set; }
+
+        public double Rating { get; set; }
+
+        //enum
+        public Genre genre { get; set; }
+
+        public Author author { get; set; }
+
+        public List<Review> reviews { get; set; }
+    }
+}

3bb4ea0ec77c0af3d2ea6441129bbca048ac2e6a
Mila <andonovikjmila@gmail.com>
2022-12-14 20:02:55 +0100

    Add controllers

---
+++ Biblio/Controllers/AuthorsController.cs
@@ -0,0 +1,93 @@
+﻿using Biblio.Data;
+using Microsoft.AspNetCore.Http;
+using Microsoft.AspNetCore.Mvc;
+
+namespace Biblio.Controllers
+{
+    public class AuthorsController : Controller
+    {
+        private readonly AppDbContext _context;
+
+        public AuthorsController(AppDbContext context)
+        {
+            _context = context;
+        }
+
+
+        // GET: AuthorsController
+        public ActionResult Index()
+        {
+            var allAuthors = _context.Authors.ToList();
+            return View(allAuthors);
+        }
+
+        // GET: AuthorsController/Details/5
+        public ActionResult Details(int id)
+        {
+            return View();
+        }
+
+        // GET: AuthorsController/Create
+        public ActionResult Create()
+        {
+            return View();
+        }
+
+        // POST: AuthorsController/Create
+        [HttpPost]
+        [ValidateAntiForgeryToken]
+        public ActionResult Create(IFormCollection collection)
+        {
+            try
+            {
+                return RedirectToAction(nameof(Index));
+            }
+            catch
+            {
+                return View();
+            }
+        }
+
+        // GET: AuthorsController/Edit/5
+        public ActionResult Edit(int id)
+        {
+            return View();
+        }
+
+        // POST: AuthorsController/Edit/5
+        [HttpPost]
+        [ValidateAntiForgeryToken]
+        public ActionResult Edit(int id, IFormCollection collection)
+        {
+            try
+            {
+                return RedirectToAction(nameof(Index));
+            }
+            catch
+            {
+                return View();
+            }
+        }
+
+        // GET: AuthorsController/Delete/5
+        public ActionResult Delete(int id)
+        {
+            return View();
+        }
+
+        // POST: AuthorsController/Delete/5
+        [HttpPost]
+        [ValidateAntiForgeryToken]
+        public ActionResult Delete(int id, IFormCollection collection)
+        {
+            try
+            {
+                return RedirectToAction(nameof(Index));
+            }
+            catch
+            {
+                return View();
+            }
+        }
+    }
+}
